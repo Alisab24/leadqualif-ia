45fb9dfca17354bba46b0329b6e26114ff65c03b
Alisab24 <aldjanari2@outlook.fr>
2026-01-01 20:12:03 +0100

    Fix: Augmentation taille password_hash pour PostgreSQL

--- backend/models.py
+++ backend/models.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(120), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
 
 class Lead(db.Model):
     id = db.Column(db.Integer, primary_key=True)
