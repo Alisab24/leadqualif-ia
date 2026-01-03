3dbad39e2b07b29b7ef22011cf841ebc397c227c
Alisab24 <aldjanari2@outlook.fr>
2026-01-03 23:06:06 +0100

    Fix: dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // L'import crucial
 import App from './App.jsx'
-import './index.css' 
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    {/* On active le moteur de routage ici pour toute l'app */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
